b25e0c09024798d3fb8faff38191a1a292b13b5d
jorgev259 <jorgev259@outlook.es>
2021-08-07 21:00:52 -0600

    Use name declared onpackage instead of path

--- src/index.js
+++ src/index.js
@@ -12,13 +12,18 @@ const global = { sequelize, client, commands, config, modules, configFile }
 const eventModules = {}
 
 async function start () {
-  await Promise.all(packages.map(async pName => {
-    const packageObj = await import(pName)
+  await Promise.all(packages.map(async pPath => {
+    const packageObj = await import(pPath)
+    const { name: pName } = packageObj
     const module = { name: pName, commandNames: [], enabled: {}, config: packageObj.config }
+    let commandSize = 0
+    let eventSize = 0
 
     for (const [name, fn] of Object.entries(packageObj.events || {})) {
       if (!eventModules[name]) eventModules[name] = [fn]
       else eventModules[name].push(fn)
+
+      eventSize++
     }
 
     for (const [name, command] of Object.entries(packageObj.commands || {})) {
@@ -27,9 +32,22 @@ async function start () {
       command.enabled = {}
       commands.set(name, command)
       module.commandNames.push(name)
+      commandSize++
     }
 
     modules.set(pName, module)
+
+    const loadedText = commandSize > 0 && eventSize > 0
+      ? ` with ${commandSize} commands and ${eventSize} events`
+      : (commandSize > 0
+          ? ` with ${commandSize} commands`
+          : (
+              eventSize > 0
+                ? ` with ${eventSize} events`
+                : ''
+            ))
+
+    console.log(`Loaded "${pName}"${loadedText}`)
   }))
 
   for (const [eventName, events] of Object.entries(eventModules)) {
