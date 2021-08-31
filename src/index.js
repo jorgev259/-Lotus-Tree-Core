7ed222362f0ac137e0ff978d193a6a54f5f9ede5
jorgev259 <jorgev259@outlook.es>
2021-08-30 22:14:58 -0600

    add preload option for modules

--- src/index.js
+++ src/index.js
@@ -17,50 +17,56 @@ const eventModules = {}
 async function start () {
   await Promise.all(packages.map(async pPath => {
     const packageObj = await import(pPath)
-    const { name: pName } = packageObj
-    const module = { name: pName, commandNames: [], enabled: {} }
-    let commandSize = 0
-    let eventSize = 0
+    const { name: pName, preload } = packageObj
+    try {
+      const module = { name: pName, commandNames: [], enabled: {} }
+      let commandSize = 0
+      let eventSize = 0
 
-    for (const [name, fn] of Object.entries(packageObj.events || {})) {
-      if (!eventModules[name]) eventModules[name] = [fn]
-      else eventModules[name].push(fn)
+      if (preload) await preload(sequelize)
 
-      eventSize++
-    }
+      for (const [name, fn] of Object.entries(packageObj.events || {})) {
+        if (!eventModules[name]) eventModules[name] = [fn]
+        else eventModules[name].push(fn)
 
-    for (const [name, command] of Object.entries(packageObj.commands || {})) {
-      command.moduleName = pName
-      command.name = name
-      command.enabled = {}
-      commands.set(name, command)
-      module.commandNames.push(name)
-      commandSize++
-    }
-
-    if (packageObj.config) {
-      for (const [name, value] of Object.entries(packageObj.config.global || {})) {
-        defaultConfig.global[name] = value
+        eventSize++
       }
 
-      for (const [name, value] of Object.entries(packageObj.config.guild || {})) {
-        defaultConfig.guild[name] = value
+      for (const [name, command] of Object.entries(packageObj.commands || {})) {
+        command.moduleName = pName
+        command.name = name
+        command.enabled = {}
+        commands.set(name, command)
+        module.commandNames.push(name)
+        commandSize++
       }
+
+      if (packageObj.config) {
+        for (const [name, value] of Object.entries(packageObj.config.global || {})) {
+          defaultConfig.global[name] = value
+        }
+
+        for (const [name, value] of Object.entries(packageObj.config.guild || {})) {
+          defaultConfig.guild[name] = value
+        }
+      }
+
+      modules.set(pName, module)
+
+      const loadedText = commandSize > 0 && eventSize > 0
+        ? ` with ${commandSize} commands and ${eventSize} events`
+        : (commandSize > 0
+            ? ` with ${commandSize} commands`
+            : (
+                eventSize > 0
+                  ? ` with ${eventSize} events`
+                  : ''
+              ))
+
+      console.log(`Loaded "${pName}"${loadedText}`)
+    } catch (err) {
+      console.log(`Failed to load ${pName} with error: ${err}`)
     }
-
-    modules.set(pName, module)
-
-    const loadedText = commandSize > 0 && eventSize > 0
-      ? ` with ${commandSize} commands and ${eventSize} events`
-      : (commandSize > 0
-          ? ` with ${commandSize} commands`
-          : (
-              eventSize > 0
-                ? ` with ${eventSize} events`
-                : ''
-            ))
-
-    console.log(`Loaded "${pName}"${loadedText}`)
   }))
 
   for (const [eventName, events] of Object.entries(eventModules)) {
